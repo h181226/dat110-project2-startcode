c60ccccdd2305134229aa667748a43d070b35153
h181226 <h181226@stud.hvl.no>
2021-03-10 10:23:28 +0100

    Implemented task A, the different sub-classes of Message

--- src/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -1,13 +1,25 @@
 package no.hvl.dat110.messages;
 
 public class DeleteTopicMsg extends Message {
+	
+	private String topic;
+	
+	public DeleteTopicMsg(String user, String topic) {
+		super(MessageType.DELETETOPIC, user);
+		this.topic = topic;
+	}
+	
+	public String getTopic() {
+		return topic;
+	}
+	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
 
-	// message sent from client to create topic on the broker
-
-	// TODO:
-	// Implement object variables - a topic is required
-
-	// Constructor, get/set-methods, and toString method
-	// as described in the project text
+	@Override
+	public String toString() {
+		return "DeleteTopicMsg [topic=" + topic + "]";
+	}
 
 }
--- src/no/hvl/dat110/messages/SubscribeMsg.java
+++ src/no/hvl/dat110/messages/SubscribeMsg.java
@@ -1,13 +1,26 @@
 package no.hvl.dat110.messages;
 
 public class SubscribeMsg extends Message {
+	
+	private String topic;
+	
+	public SubscribeMsg(String user, String topic) {
+		super(MessageType.SUBSCRIBE, user);
+		this.topic = topic;
+	}
+	
+	public String getTopic() {
+		return topic;
+	}
+	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
 
-	// message sent from client to subscribe on a topic 
+	@Override
+	public String toString() {
+		return "SubscribeMsg [topic=" + topic + "]";
+	}
 
-	// TODO:
-	// Implement object variables - a topic is required
-
-	// Constructor, get/set-methods, and toString method
-	// as described in the project text
 		
 }
